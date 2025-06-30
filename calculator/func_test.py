93052302e2d3acabeffe233d9c99fe0b16fc5614
MKNazir10 <mohamednasur10@gmail.com>
2025-06-30 12:54:17 +0530

    functional_test file added

---
+++ calculator/func_test.py
@@ -0,0 +1,20 @@
+# test_functional.py
+import unittest
+from main import sub
+
+class TestSubtractionFunctional(unittest.TestCase):
+    def test_subtraction_flow(self):
+        inputs = [(100, 50), (0, 0), (3, 7)]
+        expected_outputs = [50, 0, -4]
+
+        for (a, b), expected in zip(inputs, expected_outputs):
+            with self.subTest(a=a, b=b):
+                self.assertEqual(sub(a, b), expected)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# git rm app.py 
+# git commit -m "Removed oldfile.py"
+# git push origin main
